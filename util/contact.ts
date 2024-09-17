38a0b899c2e5a08aec28951ad1f08a5886bf16a1
Jhonny <jhonny-cwb@hotmail.com>
2024-09-17 12:57:02 -0300

    fix: wpp link

--- util/contact.ts
+++ util/contact.ts
@@ -1,8 +1,6 @@
-import { isMobile } from '@/util/device'
-
 export const openWhatsApp = () => {
 	const phone = '554199840610'
-	const endpoint = isMobile.any() ? 'api.whatsapp.com' : 'web.whatsapp.com'
+	const endpoint = 'api.whatsapp.com'
 
 	window.open(
 		`https://${endpoint}/send?phone=${phone}&text=Olá, estou no seu site e gostaria de mais informação.`
